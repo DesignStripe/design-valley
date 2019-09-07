fee6514f25ef750e33ede3dc62d9fce8b8b72a7f
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-09-07 14:55:16 +0300

    :tada: Create seperate folder for const data

---
+++ src/data/menuItems.js
@@ -0,0 +1,9 @@
+const items = [
+  { id: "colors", name: "Colors" },
+  { id: "gradients", name: "Gradients" },
+  { id: "web-accessibility", name: "Web Accessibility" },
+  { id: "design-systems", name: "Design Systems" },
+  { id: "design-tools", name: "Design Tools" }
+];
+
+export default items;
---
+++ src/data/tools.js
@@ -0,0 +1,54 @@
+const tools = {
+  colors: [
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    },
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    },
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    },
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    }
+  ],
+  gradients: [
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    }
+  ],
+  "web-accessibility": [
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    }
+  ],
+  "design-tools": [
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    }
+  ],
+  "design-systems": [
+    {
+      name: "Adobe Photoshop",
+      description: "The ultimate photo editing tool...",
+      image: "https://picsum.photos/200/"
+    }
+  ]
+};
+
+export default tools;
