536eb4b30005ed34ccb593ea1962fad5161f2e58
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-09-08 21:09:17 +0300

    :zap: Add dummy fetching methods

--- src/api/requests.js
+++ src/api/requests.js
@@ -2,10 +2,32 @@ import axios from "axios";
 import tools from "../data/tools";
 
 function fetchToolByCategory(id) {
-  console.log(id);
+  const selectedTools = tools.filter(tool => tool.category === id);
 
-  return new Promise((resolve, reject) => resolve(tools[id]));
+  return new Promise((resolve, reject) => resolve(selectedTools));
   // axios.get("");
 }
 
-export { fetchToolByCategory };
+function fetchFeatured() {
+  const selectedTools = [tools[0]];
+
+  return new Promise((resolve, reject) => resolve(selectedTools));
+  // axios.get("");
+}
+
+function fetchPopular() {
+  const selectedTools = [tools[1]];
+
+  return new Promise((resolve, reject) => resolve(selectedTools));
+  // axios.get("");
+}
+
+function fetchFavorite() {
+  // pick em from cookies...
+  const selectedTools = [tools[3], tools[4]];
+
+  return new Promise((resolve, reject) => resolve(selectedTools));
+  // axios.get("");
+}
+
+export { fetchToolByCategory, fetchFeatured, fetchPopular, fetchFavorite };
