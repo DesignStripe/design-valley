875953ed807f74485febae648e05d076d765e5da
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-09-14 11:34:11 +0300

    :lipstick: Update button style

--- src/components/Card.js
+++ src/components/Card.js
@@ -65,15 +65,42 @@ const Description = styled.p`
 `;
 
 const CustomButton = styled(Button)`
-  background-color: lightgrey;
-  color: #212121;
-  border-radius: 8px;
+  cursor: pointer;
+  display: inline-block;
+  min-height: 1em;
+  outline: 0;
+  border: none;
+  vertical-align: baseline;
+  background: #e0e1e2 none;
+  color: rgba(0, 0, 0, 0.6);
+  margin: 0 0.25em 0 0;
+  padding: 0.78571429em 1.5em 0.78571429em;
+  text-transform: none;
+  text-shadow: none;
+  font-weight: 700;
+  line-height: 1em;
+  font-style: normal;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 0.28571429rem;
 
-  font-size: 1rem;
+  box-shadow: 0 0 0 1px transparent inset, 0 0 0 0 rgba(34, 36, 38, 0.15) inset;
+  user-select: none;
+  transition: opacity 0.1s ease, background-color 0.1s ease, color 0.1s ease,
+    background 0.1s ease, -webkit-box-shadow 0.1s ease;
+  transition: opacity 0.1s ease, background-color 0.1s ease, color 0.1s ease,
+    box-shadow 0.1s ease, background 0.1s ease, -webkit-box-shadow 0.1s ease;
+
   padding: 12px 0;
   font-family: Rubik, "sans-serif";
-  font-weight: bold;
-  box-shadow: none;
+
+  &:hover {
+    background-color: #cacbcd;
+    background-image: none;
+    box-shadow: 0 0 0 1px transparent inset,
+      0 0 0 0 rgba(34, 36, 38, 0.15) inset;
+    color: rgba(0, 0, 0, 0.8);
+  }
 `;
 
 const Row = styled.div`
