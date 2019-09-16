0fc125fe8978f7c37132224dcc41bb2da38d9ccd
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-09-16 18:07:35 +0300

    :sparkles: Populate category field of tools

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,25 +1,43 @@
 import axios from "axios";
 import tools from "../data/tools";
+import categories from "../data/categories";
 import { getFavorites } from "../utils/localStorage";
+
+function findCategory(id) {
+  return categories.find(category => category.id === id);
+}
+
+function populateCategory(arr) {
+  return arr.map(item => {
+    const categoryId = item.category;
+    item.category = findCategory(categoryId);
+    return item;
+  });
+}
 
 function fetchToolByCategory(id) {
   const selectedTools = tools.filter(tool => tool.category === id);
 
-  return new Promise((resolve, reject) => resolve(selectedTools));
-  // axios.get("");
+  return new Promise((resolve, reject) =>
+    resolve(populateCategory(selectedTools))
+  );
 }
 
 function fetchFeatured() {
   const selectedTools = [tools[0]];
 
-  return new Promise((resolve, reject) => resolve(selectedTools));
+  return new Promise((resolve, reject) =>
+    resolve(populateCategory(selectedTools))
+  );
   // axios.get("");
 }
 
 function fetchPopular() {
   const selectedTools = [tools[1]];
 
-  return new Promise((resolve, reject) => resolve(selectedTools));
+  return new Promise((resolve, reject) =>
+    resolve(populateCategory(selectedTools))
+  );
   // axios.get("");
 }
 
@@ -29,10 +47,10 @@ function fetchFavorite() {
   const favoriteTools = favoritesIds.map(favoriteId =>
     tools.find(tool => tool.id === favoriteId)
   );
-  console.log(favoriteTools);
 
-  return new Promise((resolve, reject) => resolve(favoriteTools));
-  // axios.get("");
+  return new Promise((resolve, reject) =>
+    resolve(populateCategory(favoriteTools))
+  );
 }
 
 export { fetchToolByCategory, fetchFeatured, fetchPopular, fetchFavorite };
