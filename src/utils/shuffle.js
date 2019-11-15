6f7d160676b6c34d6fa85420261d707723cc2ca2
Dimitris Raptis <dimitrisraptis96@gmail.com>
2019-11-15 20:44:18 +0200

    :tada: Create a function to shuffle an array

---
+++ src/utils/shuffle.js
@@ -0,0 +1,18 @@
+// https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
+
+export default shuffle;
